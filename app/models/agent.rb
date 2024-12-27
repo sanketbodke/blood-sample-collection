2f9add9c720083f071637f2ff261e180ab0a1086
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2024-12-27 23:36:12 +0530

    Patient appointment table add

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,14 +1,11 @@
 class Agent < ApplicationRecord
   belongs_to :lab
   has_one :address, as: :addressable, dependent: :destroy
+  has_many :patientAppointments
   accepts_nested_attributes_for :address
   before_save :set_latitude_and_longitude
 
   scope :recently_updated, -> { order(updated_at: :desc) }
-
-  validates :name, presence: true, length: { minimum: 2, maximum: 100 }
-  validates :phone, presence: true, format: { with: /\A\d{10}\z/, message: "must be a valid 10-digit phone number" }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 
   private
   def set_latitude_and_longitude
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_one :lab, dependent: :destroy
   has_one :address, as: :addressable, dependent: :destroy
+  has_many :patientAppointments
   accepts_nested_attributes_for :address
 
   rolify
